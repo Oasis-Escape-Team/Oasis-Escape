e686b5927f135b7a3eff5c5e8adfa46ed7c6dad0
Abhishek Jadaun <am3239@srmist.edu.in>
2024-04-14 22:20:44 +0530

    Final commit

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const mongoURI = "mongodb://127.0.0.1:27017/airstay"
+// mongodb+srv://singhshivanshu455:humma1234@shivanshuapi.74ycbsv.mongodb.net/Shivanshuapi?retryWrites=true&w=majority
 
 const connectToMongo=()=>{
     mongoose.connect(mongoURI);
